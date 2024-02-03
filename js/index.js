af72ee706975541414c3b4c60c9756e5dbaa1914
Lucas Cubile <cubilelucas@gmail.com>
2024-02-03 17:20:15 -0300

    Changed if else block to switch cases

--- js/index.js
+++ js/index.js
@@ -1,63 +1,68 @@
 'use strict'
 
-const getComputerChoice = () => {
-  let choice = Math.ceil(Math.random() * 3)
-  if (choice === 1) return 'Rock'
-  else if (choice === 2) return 'Paper'
-  else return 'Scissors'
-}
-
-const playRound = (playerSelection, computerSelection) => {
-  if (playerSelection === computerSelection) {
-    return "It's a tie"
-  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    return 'You win! Rock beats scissors'
-  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return 'You lose! Paper beats Rock'
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return 'You win! Paper beats rock'
-  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    return 'You lose! Scissors beats paper'
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return 'You win! Scissors beats paper'
-  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    return 'You lose! Rock beats Scissors'
+window.addEventListener('load', () => {
+  const getComputerChoice = () => {
+    let choice = Math.ceil(Math.random() * 3)
+    if (choice === 1) return 'Rock'
+    else if (choice === 2) return 'Paper'
+    else return 'Scissors'
   }
-}
 
-const ROCK = 'Rock'
-const PAPER = 'Paper'
-const SCISSORS = 'Scissors'
-let myScore = 0
-let computerScore = 0
+  const playRound = (playerSelection, computerSelection) => {
+    switch (true) {
+      case playerSelection === computerSelection:
+        return "It's a tie"
+      case playerSelection === 'rock' && computerSelection === 'scissors':
+        return 'You win! Rock beats Scissors'
+      case playerSelection === 'rock' && computerSelection === 'paper':
+        return 'You lose! Paper beats rock'
+      case playerSelection === 'paper' && computerSelection === 'rock':
+        return 'You win! Paper beats rock'
+      case playerSelection === 'paper' && computerSelection === 'scissors':
+        return 'You lose! Scissors beats paper'
+      case playerSelection === 'scissors' && computerSelection === 'paper':
+        return 'You win! Scissors beats paper'
+      case playerSelection === 'scissors' && computerSelection === 'rock':
+        return 'You lose! Rock beats Scissors'
+      default:
+        return 'Invalid Selection'
+    }
+  }
 
-const playGame = () => {
-  while (myScore < 5 && computerScore < 5) {
-    let myChoice = prompt('Choose Rock, Paper or Scissors').toLowerCase()
-    while (myChoice !== ROCK.toLowerCase() && myChoice !== PAPER.toLowerCase() && myChoice !== SCISSORS.toLowerCase()) {
-      console.log('You must choose among Rock, Paper or Scissors')
-      myChoice = prompt('You must choose among Rock, Paper or Scissors')
+  const ROCK = 'Rock'
+  const PAPER = 'Paper'
+  const SCISSORS = 'Scissors'
+  let myScore = 0
+  let computerScore = 0
+
+  const playGame = () => {
+    while (myScore < 5 && computerScore < 5) {
+      let myChoice = prompt('Choose Rock, Paper or Scissors').toLowerCase()
+      while (myChoice !== ROCK.toLowerCase() && myChoice !== PAPER.toLowerCase() && myChoice !== SCISSORS.toLowerCase()) {
+        console.log('You must choose among Rock, Paper or Scissors')
+        myChoice = prompt('You must choose among Rock, Paper or Scissors')
+      }
+
+      let pcChoice = getComputerChoice().toLowerCase()
+      let result = playRound(myChoice, pcChoice)
+      if (result.includes('You lose')) {
+        computerScore++
+        console.log(`${result}. Your score is ${myScore} and computer's is ${computerScore}`)
+      } else if (result.includes('You win')) {
+        myScore++
+        console.log(`${result}. Your score is ${myScore} and computer's is ${computerScore}`)
+      } else {
+        console.log(`${result}. Your score is ${myScore} and computer's is ${computerScore}`)
+        continue
+      }
     }
 
-    let pcChoice = getComputerChoice().toLowerCase()
-    let result = playRound(myChoice, pcChoice)
-    if (result.includes('You lose')) {
-      computerScore++
-      console.log(`${result}. Your score is ${myScore} and computer's is ${computerScore}`)
-    } else if (result.includes('You win')) {
-      myScore++
-      console.log(`${result}. Your score is ${myScore} and computer's is ${computerScore}`)
+    if (myScore === 5) {
+      console.log('Congrats you won the game')
     } else {
-      console.log(`${result}. Your score is ${myScore} and computer's is ${computerScore}`)
-      continue
+      console.log('You lost the game')
     }
   }
 
-  if (myScore === 5) {
-    console.log('Congrats you won the game')
-  } else {
-    console.log('You lost the game')
-  }
-}
-
-playGame()
+  playGame()
+})
