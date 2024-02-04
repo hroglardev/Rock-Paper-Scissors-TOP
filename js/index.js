56705e79bbb10ae3dde0248759340660284e7284
Lucas Cubile <cubilelucas@gmail.com>
2024-02-04 16:19:33 -0300

    Add images to HTML. Apply styles for UI. Add filter interactivity with JS

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ window.addEventListener('load', () => {
     else if (choice === 2) return 'Paper'
     else return 'Scissors'
   }
+  const buttons = Array.from(document.querySelectorAll('button'))
+
+  const rock = document.querySelector('.rock')
+  const paper = document.querySelector('.paper')
+  const scissors = document.querySelector('.scissors')
 
   const playRound = (playerSelection, computerSelection) => {
     switch (true) {
@@ -14,21 +19,57 @@ window.addEventListener('load', () => {
         return "It's a tie"
 
       case playerSelection === 'Rock' && computerSelection === 'Scissors':
+        paper.classList.add('filter')
+        scissors.classList.add('filter')
+        setTimeout(() => {
+          paper.classList.remove('filter')
+          scissors.classList.remove('filter')
+        }, 1000)
         return 'You win! Rock beats Scissors'
 
       case playerSelection === 'Rock' && computerSelection === 'Paper':
-        return 'You lose! Paper beats rock'
+        paper.classList.add('filter')
+        scissors.classList.add('filter')
+        setTimeout(() => {
+          paper.classList.remove('filter')
+          scissors.classList.remove('filter')
+        }, 1000)
+        return 'You lose! Paper beats Rock'
 
       case playerSelection === 'Paper' && computerSelection === 'Rock':
-        return 'You win! Paper beats rock'
+        scissors.classList.add('filter')
+        rock.classList.add('filter')
+        setTimeout(() => {
+          rock.classList.remove('filter')
+          scissors.classList.remove('filter')
+        }, 1000)
+        return 'You win! Paper beats Rock'
 
       case playerSelection === 'Paper' && computerSelection === 'Scissors':
-        return 'You lose! Scissors beats paper'
+        rock.classList.add('filter')
+        scissors.classList.add('filter')
+        setTimeout(() => {
+          rock.classList.remove('filter')
+          scissors.classList.remove('filter')
+        }, 1000)
+        return 'You lose! Scissors beats Paper'
 
       case playerSelection === 'Scissors' && computerSelection === 'Paper':
-        return 'You win! Scissors beats paper'
+        paper.classList.add('filter')
+        rock.classList.add('filter')
+        setTimeout(() => {
+          paper.classList.remove('filter')
+          rock.classList.remove('filter')
+        }, 1000)
+        return 'You win! Scissors beats Paper'
 
       case playerSelection === 'Scissors' && computerSelection === 'Rock':
+        paper.classList.add('filter')
+        rock.classList.add('filter')
+        setTimeout(() => {
+          paper.classList.remove('filter')
+          rock.classList.remove('filter')
+        }, 1000)
         return 'You lose! Rock beats Scissors'
 
       default:
@@ -43,7 +84,6 @@ window.addEventListener('load', () => {
 
   const userScore = document.querySelector('.myScore')
   const pcScore = document.querySelector('.computer-score')
-  const buttons = Array.from(document.querySelectorAll('button'))
 
   const resetGame = () => {
     myScore = 0
@@ -53,10 +93,15 @@ window.addEventListener('load', () => {
     pcScore.innerText = `Computer's score is ${computerScore}`
     body.removeChild(resetButton)
     buttons.forEach((button) => button.removeAttribute('disabled'))
+    rock.classList.remove('filter')
+    paper.classList.remove('filter')
+    scissors.classList.remove('filter')
   }
 
   const resetButton = document.createElement('button')
   resetButton.innerText = 'Play again'
+  resetButton.classList.add('button')
+  resetButton.classList.add('reset')
   resetButton.addEventListener('click', resetGame)
 
   buttons.forEach((button) => {
@@ -73,6 +118,7 @@ window.addEventListener('load', () => {
       } else {
         message.innerText = roundResult
       }
+
       if (myScore === 5 || computerScore === 5) {
         body.appendChild(resetButton)
         message.innerText = myScore === 5 ? 'Congrats, you won the game' : "Sorry, you've lost to the pc"
